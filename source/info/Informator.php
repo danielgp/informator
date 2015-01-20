e83184e4838cb824bab33862eec1da6cb186f5c4
Daniel Popiniuc <danielpopiniuc@gmail.com>
2015-01-20 22:22:37 +0200

    optimized to use more functions from common-lib (mine)

--- source/info/Informator.php
+++ source/info/Informator.php
@@ -34,14 +34,12 @@ class Informator extends AppQueries
     use \danielgp\common_lib\CommonCode;
 
     private $filesFromDir;
-    protected $mySQLconnection = null;
 
     public function __construct()
     {
         $knownLabels = [
             'ApacheInfo'           => $this->getApacheDetails(),
             'ClientInfo'           => $this->getClientBrowserDetails(),
-            'ListOfFiles'          => $this->getListOfFiles(realpath('.')),
             'MySQLactiveDatabases' => $this->getMySQLactiveDatabases(),
             'MySQLactiveEngines'   => $this->getMySQLactiveEngines(),
             'MySQLgenericInfo'     => $this->getMySQLgenericInformations(),
@@ -84,28 +82,15 @@ class Informator extends AppQueries
      */
     private function getMySQLactiveDatabases()
     {
-        $this->connectToMySql();
+        $this->connectToMySqlServer();
         if (is_null($this->mySQLconnection)) {
             echo '<p style="color:red;">'
             . 'There is no connection to MySQL server, '
             . 'therefore I cannot asses the MySQL active user databases '
             . '(with function `' . __FUNCTION__ . '`)...</p>';
             return null;
-        }
-        $result = $this->mySQLconnection->query($this->sActiveDatabases());
-        if ($result) {
-            $iNoOfRows = $result->num_rows;
-            for ($counter = 0; $counter < $iNoOfRows; $counter++) {
-                $line[] = $result->fetch_assoc();
-            }
-            $result->close();
         } else {
-            $line['error'] = 'In functia <b>'
-                . __FUNCTION__ . '</b>'
-                . ', nu se poate efectua interogarea '
-                . '<b>' . $query . '</b>'
-                . ' because <b>' . $this->mySQLconnection->error
-                . '<b/>...';
+            $line = $this->setMySQLquery2Server($this->sActiveDatabases(), 'full_array_key_numbered')['result'];
         }
         return $line;
     }
@@ -117,35 +102,22 @@ class Informator extends AppQueries
      */
     private function getMySQLactiveEngines()
     {
-        $this->connectToMySql();
+        $this->connectToMySqlServer();
         if (is_null($this->mySQLconnection)) {
             echo '<p style="color:red;">'
             . 'There is no connection to MySQL server, '
             . 'therefore I cannot asses the MySQL active engines '
             . '(with function `' . __FUNCTION__ . '`)...</p>';
             return null;
-        }
-        $result = $this->mySQLconnection->query($this->sActiveEngines());
-        if ($result) {
-            $iNoOfRows = $result->num_rows;
-            for ($counter = 0; $counter < $iNoOfRows; $counter++) {
-                $line[] = $result->fetch_assoc();
-            }
-            $result->close();
         } else {
-            $line['erorr'] = 'In functia <b>'
-                . __FUNCTION__ . '</b>'
-                . ', nu se poate efectua interogarea '
-                . '<b>' . $query . '</b>'
-                . ' because <b>' . $this->mySQLconnection->error
-                . '<b/>...';
+            $line = $this->setMySQLquery2Server($this->sActiveEngines(), 'full_array_key_numbered')['result'];
         }
         return $line;
     }
 
     private function getMySQLgenericInformations()
     {
-        $this->connectToMySql();
+        $this->connectToMySqlServer();
         if (is_null($this->mySQLconnection)) {
             echo '<p style="color:red;">'
             . 'There is no connection to MySQL server, '
@@ -166,31 +138,17 @@ class Informator extends AppQueries
      */
     private function getMySQLglobalVariables()
     {
-        $this->connectToMySql();
+        $this->connectToMySqlServer();
         if (is_null($this->mySQLconnection)) {
             echo '<p style="color:red;">'
             . 'There is no connection to MySQL server, '
             . 'therefore I cannot asses the MySQL global variable values '
             . '(with function `' . __FUNCTION__ . '`)...</p>';
             return null;
-        }
-        $query  = 'SHOW GLOBAL VARIABLES;';
-        $result = $this->mySQLconnection->query($query);
-        if ($result) {
-            $iNoOfRows = $result->num_rows;
-            for ($counter = 0; $counter < $iNoOfRows; $counter++) {
-                $line                   = $result->fetch_row();
-                $array2return[$line[0]] = $line[1];
-            }
-            $result->close();
         } else {
-            $line['error'] = 'In functia <b>'
-                . __FUNCTION__ . '</b>'
-                . ', nu se poate efectua interogarea '
-                . '<b>' . $query . '</b>'
-                . ' because <b>' . $this->mySQLconnection->error . '<b/>...';
+            $line = $this->setMySQLquery2Server('SHOW GLOBAL VARIABLES;', 'array_key_value')['result'];
         }
-        return $array2return;
+        return $line;
     }
 
     private function getMySQLinfo()
@@ -273,34 +231,17 @@ class Informator extends AppQueries
         ];
     }
 
-    protected function connectToMySql()
+    protected function connectToMySqlServer()
     {
         if (is_null($this->mySQLconnection)) {
-            $mySQLconfig           = [
+            $mySQLconfig = [
                 'host'     => MYSQL_HOST,
                 'port'     => MYSQL_PORT,
-                'user'     => MYSQL_USERNAME,
+                'username' => MYSQL_USERNAME,
                 'password' => MYSQL_PASSWORD,
                 'database' => MYSQL_DATABASE,
             ];
-            extract($mySQLconfig);
-            $this->mySQLconnection = new \mysqli($host, $user, $password, $database, $port);
-            if ($this->mySQLconnection->connect_error) {
-                $erNo                  = $this->mySQLconnection->connect_errno;
-                $erMsg                 = $this->mySQLconnection->connect_error;
-                $feedback              = implode('', [
-                    'Connection error (',
-                    'no.: %d, ',
-                    'message from server: %s, ',
-                    'host = %s, ',
-                    'port: %s, ',
-                    'username: %s, ',
-                    'database: %s',
-                    ')'
-                ]);
-                echo sprintf($feedback, $erNo, $erMsg, $host, $port, $user, $database);
-                $this->mySQLconnection = null;
-            }
+            $this->connectToMySql($mySQLconfig);
         }
     }
 
@@ -447,74 +388,5 @@ class Informator extends AppQueries
                 'OS'      => $osInfo,
             ];
         }
-    }
-
-    /**
-     * returns the details about Communicator (current) file
-     *
-     * @return array
-     */
-    private function getFileDetails($fileGiven)
-    {
-        return [
-            'Name'                      => $fileGiven,
-            'Size'                      => filesize($fileGiven),
-            'Sha1'                      => sha1_file($fileGiven),
-            'TimestampAccessed'         => fileatime($fileGiven),
-            'TimestampAccessedReadable' => date('Y-m-d H:i:s', fileatime($fileGiven)),
-            'TimestampChanged'          => filectime(__FILE__),
-            'TimestampChangedReadable'  => date('Y-m-d H:i:s', filectime($fileGiven)),
-            'TimestampModified'         => filemtime(__FILE__),
-            'TimestampModifiedReadable' => date('Y-m-d H:i:s', filemtime($fileGiven)),
-        ];
-    }
-
-    /**
-     * returns a multi-dimensional array with list of file details within a given path
-     * @param  string $pathAnalised
-     * @return array
-     */
-    private function getListOfFiles($pathAnalised)
-    {
-        if (!file_exists($pathAnalised)) {
-            return null;
-        }
-        $dir                = dir($pathAnalised);
-        $this->filesFromDir = 0;
-        $fileDetails        = null;
-        while ($file               = $dir->read()) {
-            clearstatcache();
-            $fName     = $pathAnalised . '/' . $file;
-            $fileParts = pathinfo($fName);
-            switch ($fileParts['basename']) {
-                case '.':
-                case '..':
-                    break;
-                default:
-                    if (is_dir($fName)) {
-                        $fileDetails[$fName] = $this->getListOfFiles($fName);
-                    } else {
-                        $this->filesFromDir += 1;
-                        $xt                  = (isset($fileParts['extension']) ? $fileParts['extension'] : '-');
-                        $fileDetails[$fName] = [
-                            'Folder'                    => $fileParts['dirname'],
-                            'BaseName'                  => $fileParts['basename'],
-                            'Extension'                 => $xt,
-                            'FileName'                  => $fileParts['filename'],
-                            'Size'                      => filesize($fName),
-                            'Sha1'                      => sha1_file($fName),
-                            'TimestampAccessed'         => fileatime($fName),
-                            'TimestampAccessedReadable' => date('Y-m-d H:i:s', fileatime($fName)),
-                            'TimestampChanged'          => filectime($fName),
-                            'TimestampChangedReadable'  => date('Y-m-d H:i:s', filectime($fName)),
-                            'TimestampModified'         => filemtime($fName),
-                            'TimestampModifiedReadable' => date('Y-m-d H:i:s', filemtime($fName)),
-                        ];
-                    }
-                    break;
-            }
-        }
-        $dir->close();
-        return $fileDetails;
     }
 }
